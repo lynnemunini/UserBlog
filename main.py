89efeafd534daa9efcdf588f5057f7279ab3c7cc
Lynne Munini <muninilynne65@gmail.com>
2022-01-18 15:13:22 -0500

    Added relational databases

--- main.py
+++ main.py
@@ -30,22 +30,30 @@ login_manager.init_app(app)
 ##CONFIGURE TABLES
 
 class User(UserMixin, db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+        
+    #This will act like a List of BlogPost objects attached to each User. 
+    #The "author" refers to the author property in the BlogPost class.
+    posts = relationship("BlogPost", back_populates="author")
 # db.create_all()
 
 class BlogPost(db.Model):
     __tablename__ = "blog_posts"
     id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.String(250), nullable=False)
+    #Create Foreign Key, "users.id" the users refers to the tablename of User.
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    #Create reference to the User object, the "posts" refers to the posts protperty in the User class.
+    author = relationship("User", back_populates="posts")
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
-# db.create_all()
+db.create_all()
 
 ##WTForm
 class RegisterForm(FlaskForm):
@@ -166,7 +174,7 @@ def add_new_post():
                 subtitle=form.subtitle.data,
                 body=form.body.data,
                 img_url=form.img_url.data,
-                author=current_user.name,
+                author=current_user,
                 date=date.today().strftime("%B %d, %Y")
             )
             db.session.add(new_post)
